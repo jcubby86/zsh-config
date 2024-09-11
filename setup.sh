5e715099d94ae1f517b5932f20122a2089779be6
Jacob Bastian <91275439+jcubby86@users.noreply.github.com>
2024-09-11 17:16:42 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ then
     exit 1
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
@@ -16,3 +18,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 
 cp .zshrc $HOME/.zshrc
 cp .p10k.zsh $HOME/.p10k.zsh
+
+if user_can_sudo; then
+    sudo -k chsh -s "$zsh" "$USER"  # -k forces the password prompt
+else
+    chsh -s "$zsh" "$USER"          # run chsh normally
+fi
+exec zsh -l
